1aa00660a051eac08669ab9a18ef844a2c8b11eb
иван Горбунов <isdfdsf.sdfs@mail,ru>
2025-10-25 18:31:38 +0400

    пофиксил ошибку с запуском storybook-а, сократил название скрипта

--- src/components/button/VuButton.vue
+++ src/components/button/VuButton.vue
@@ -1,7 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// Определяем пропсы компонента
+interface Props {
+  text?: string;
+}
+
+// Объявляем пропсы
+defineProps<Props>();
+</script>
 
 <template>
-	<v-btn class="text-body-1 font-weight-bold" color="brand-orange-4-notif" text="Привет"></v-btn>
+  <v-btn 
+    class="text-body-1 font-weight-bold" 
+    color="brand-orange-4-notif" 
+    :text="text"
+  ></v-btn>
 </template>
 
 <style lang="css" scoped></style>
---
+++ src/components/checkBox/VuCheckbox.vue
@@ -0,0 +1,7 @@
+<script>
+let isChecked = false
+</script>
+
+<template>
+    <v-checkbox v-model="isChecked" label="Checkbox"></v-checkbox>
+</template>
