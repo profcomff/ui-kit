bbbe9bac7c61e1310d79eeb409111fed2dc35169
иван Горбунов <isdfdsf.sdfs@mail,ru>
2025-10-26 16:08:20 +0400

    Реализовал компонент VuCheckbox, но без тейлвинд

--- src/components/checkBox/VuCheckbox.stories.ts
+++ src/components/checkBox/VuCheckbox.stories.ts
@@ -1,13 +1,30 @@
 import { Meta, StoryObj } from '@storybook/vue3-vite';
-import VuCheckbox from './VuCheckbox.vue'
+import VuCheckbox from './VuCheckbox.vue';
 
-const meta =  {
-    title: "checkBox/VuCheckbox",
-    component: VuCheckbox
-}satisfies Meta<typeof VuCheckbox>;
+const meta = {
+	title: 'checkBox/VuCheckbox',
+	component: VuCheckbox,
+} satisfies Meta<typeof VuCheckbox>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Default: Story = {
+	args: {
+		disabled: false,
+		modelValue: false,
+	},
+};
+export const Active: Story = {
+	args: {
+		disabled: false,
+		modelValue: true,
+	},
+};
+export const Disabled: Story = {
+	args: {
+		disabled: true,
+		modelValue: false,
+	},
+};
--- src/components/checkBox/VuCheckbox.vue
+++ src/components/checkBox/VuCheckbox.vue
@@ -1,7 +1,23 @@
-<template>
-    <v-checkbox label="Оценок нет" class=""> </v-checkbox>
-</template>
-
 <script setup lang="ts">
+import { computed } from 'vue';
 
+    interface Props {
+        disabled: boolean;
+        modelValue: boolean;
+    }
+    const props = defineProps<Props>();
+    const emit = defineEmits(['update:modelValue'])
+
+    const value = computed({
+        get: () => props.modelValue,
+        set: (value) => emit('update:modelValue', value)
+    })
 </script>
+
+<template>
+	<v-checkbox 
+        label="Оценок нет" 
+        v-model="value"
+        :disabled="disabled"> 
+    </v-checkbox>
+</template>
