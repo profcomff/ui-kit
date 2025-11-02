c0374751fc45172e9fc48439e43fb4f194acb2ca
Dmatrushka19 <dmasik1903@gmail.com>
2025-11-02 21:46:48 +0300

    упростил

--- src/components/VuSearch.vue
+++ src/components/VuSearch.vue
@@ -20,52 +20,42 @@ const isActive = ref(false);
 </script>
 
 <template>
-	<div class="vu-search" :class="{ hovered: isHovered, active: isActive }">
-		<v-icon class="search-icon" icon="mdi-magnify" />
-		<v-text-field
-			v-model="innerValue"
-			placeholder="Поиск"
-			density="compact"
-			variant="outlined"
-			hide-details
-			@mouseenter="isHovered = true"
-			@mouseleave="isHovered = false"
-			@mousedown="isActive = true"
-			@mouseup="isActive = false"
-			class="search-input"
-		/>
-		<v-icon class="filter-icon" icon="mdi-filter-variant" @click="emit('click-filter')" />
-	</div>
+	<v-text-field
+		v-model="innerValue"
+		placeholder="Поиск преподавателя"
+		hide-details
+		density="compact"
+		variant="outlined"
+		class="vu-search text-body-1"
+		prepend-inner-icon="mdi-magnify"
+		append-inner-icon="mdi-filter-variant"
+		color="gray-4-text-border"
+		@click:append-inner="emit('click-filter')"
+		@mouseenter="isHovered = true"
+		@mouseleave="isHovered = false"
+		@mousedown="isActive = true"
+		@mouseup="isActive = false"
+	/>
 </template>
 
 <style scoped>
 .vu-search {
-	display: flex;
-	align-items: center;
-	border: 1px solid var(--v-theme-gray-4-text-border);
+	width: 375px;
+	height: 41px;
 	border-radius: 8px;
-	padding: 4px 8px;
-	gap: 4px;
+	border: 1px solid var(--v-theme-gray-4-text-border); /* default #949494 */
 	transition:
 		border-color 0.2s,
 		border-width 0.2s;
 
-	&.hovered {
+	&:hover {
 		border-color: var(--v-theme-base-black);
 		border-width: 1px;
 	}
 
-	&.active {
+	&:active {
 		border-color: var(--v-theme-base-black);
 		border-width: 2px;
 	}
-}
-
-.search-input {
-	flex-grow: 1;
-}
-
-.filter-icon {
-	cursor: pointer;
 }
 </style>
