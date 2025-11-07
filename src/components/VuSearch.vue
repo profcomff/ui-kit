f4a5b2d422bb773080f2213733bb7c5e57bc117c
Dmatrushka19 <dmasik1903@gmail.com>
2025-11-07 16:47:17 +0300

    убрал лишние стили

--- src/components/VuSearch.vue
+++ src/components/VuSearch.vue
@@ -20,21 +20,8 @@ defineEmits<{
 	/>
 </template>
 
-<style scoped>
+<style lang="css" scoped>
 .vu-search {
 	width: 375px;
 }
-/* stylelint-disable selector-class-pattern */
-
-/* Стилизация иконок */
-.vu-search ::v-deep(.v-input__icon--prepend-inner .v-icon),
-.vu-search ::v-deep(.v-input__icon--append-inner .v-icon) {
-	color: var(--v-theme-gray-4-text-border);
-}
-
-/* Стилизация черной рамки в состоянии 'active' (focus) */
-.vu-search.v-field--focused ::v-deep(.v-field__outline) {
-	color: var(--v-theme-base-black);
-}
-/* stylelint-enable selector-class-pattern */
 </style>
