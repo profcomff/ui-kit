23db4d42140ef6c698bffad82a220e475df47dd5
didu4 <solosar42@gmail.com>
2025-12-17 21:12:13 +0400

    реализовал нужный крестик, пробно подобрал нужный цвет для дефолт и онклик состояний; возникли проблемы с цветом шрифта у соответствующих состояний

--- src/components/Tag/VuTag.vue
+++ src/components/Tag/VuTag.vue
@@ -16,6 +16,7 @@ defineEmits<{
 <template>
 	<v-chip
 		class="text-body-2"
+		:color="active ? 'primary' : danger ? 'error' : 'surface-variant'"
 		:class="{
 			'vu-tag-active': active,
 			'vu-tag-danger': danger,
@@ -26,12 +27,21 @@ defineEmits<{
 		@click:close="$emit('close')"
 	>
 		{{ text }}
+		<template v-if="closable" #close>
+			<v-icon icon="mdi-close" size="16" @click.stop="$emit('close')" />
+		</template>
 	</v-chip>
 </template>
 
 <style scoped>
+.vu-tag {
+	color: rgb(0 0 0 / 100%) !important;
+}
 
-
+.vu-tag-active,
+.vu-tag-danger {
+	color: rgb(255 255 255) !important;
+}
 
 .vu-tag-active {
 	background-color: rgb(var(--v-theme-primary));
