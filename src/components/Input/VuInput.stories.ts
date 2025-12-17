c094b3521edcc0fe7f15bb8d81a45052ba5d7369
иван Горбунов <isdfdsf.sdfs@mail,ru>
2025-12-17 20:18:35 +0400

    исправления после code review

--- src/components/Input/VuInput.stories.ts
+++ src/components/Input/VuInput.stories.ts
@@ -2,29 +2,30 @@ import { Meta, StoryObj } from '@storybook/vue3-vite';
 import VuInput from './VuInput.vue';
 
 const meta = {
-	title: 'Viribus Unitis/VuInput',
-	component: VuInput,
+  title: 'Viribus Unitis/VuInput',
+  component: VuInput,
+  tags: ['autodocs'],
+  args: {
+    message: 'Номер группы',
+    variant: 'outlined',
+  },
 } satisfies Meta<typeof VuInput>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-	args: {
-		message: 'Номер группы',    
-	},
+export const Default: Story = {};
+
+export const WithPlaceholder: Story = {
+  args: {
+    placeholder: "Введите номер группы",
+    autofocus: true
+  },
 };
-export const Hover: Story = {
-	args: {
-		message: 'Номер группы', 
-         rules: [(value: string) => !!value || 'Обязательное поле']  
-	},
-};
-export const Active: Story = {
-	args: {
-		message: 'Номер группы',
-        placeholder:"Введите номер группы",
-        autofocus: true
-	},
+
+export const Validate: Story = {
+  args: {
+    rules: [(value: string) => !!value || 'Обязательное поле']
+  },
 };
--- src/components/Input/VuInput.vue
+++ src/components/Input/VuInput.vue
@@ -1,49 +1,45 @@
 <script setup lang="ts">
 import { PropType } from 'vue';
 
-type VariantType = "outlined" | "filled" | "underlined" | "plain" | "solo" | "solo-inverted" | "solo-filled"
+type VariantType = "outlined"
 type Rule = (value: any) => string | boolean;
 
 defineProps({
-	message: {
-		type: String,
-		default: ""
-	},
-	color: {
-		type: String,
-		default: "#000000"
-	},
-	variant: {
-		type: String as PropType<VariantType>,
-		default: ""
-	},
-	placeholder: {
-		type: String,
-		default: ""
-	},
-	autofocus: {
-		type: Boolean,
-		default: false
-	},
-	rules: {
-		type: Array as PropType<Rule[]>,
-		default: () => []
-	}
+  message: {
+    type: String,
+    default: ""
+  },
+  variant: {
+    type: String as PropType<VariantType>,
+    default: "outlined"
+  },
+  placeholder: {
+    type: String,
+    default: ""
+  },
+  autofocus: {
+    type: Boolean,
+    default: false
+  },
+  rules: {
+    type: Array as PropType<Rule[]>,
+    default: () => []
+  }
 });
 
 defineEmits(['update:modelValue']);
 </script>
 
 <template>
-	<v-text-field
-		max-width="348px"
-		type="input"
-		:label="message"
-		:color="color"
-		:variant="variant || undefined"
-		:placeholder="placeholder"
-		:autofocus="autofocus"
-		:rules="rules"
-	>
-	</v-text-field>
+  <v-text-field
+    max-width="348px"
+    type="input"
+    :label="message"
+    color="primary"
+    :variant="variant"
+    :placeholder="placeholder"
+    :autofocus="autofocus"
+    :rules="rules"
+  >
+  </v-text-field>
 </template>
