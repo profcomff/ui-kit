130d4f0d951101dd9bfa4b3250aafecace5b28c9
Илья Батуев <batuev.io18@physics.msu.ru>
2026-03-18 15:31:42 +0300

    fit the main to content

--- .storybook/StoryWrapper.vue
+++ .storybook/StoryWrapper.vue
@@ -3,8 +3,14 @@
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
 	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100..900&display=swap" rel="stylesheet" />
 	<v-app>
-		<v-main>
+		<v-main class="h-fit!">
 			<slot name="story"></slot>
 		</v-main>
 	</v-app>
 </template>
+
+<style lang="css">
+.v-application__wrap {
+	min-height: 0vh !important;
+}
+</style>
